7c63e97cbf7dfb83b919484129461d3fc91e66cd
Ahiabuike Excel Chukwuka <ahiabuikeexcel@gmail.com>
2025-06-20 19:38:21 +0100

    feat: add migration route for Railway

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations ran successfully!';
+})
